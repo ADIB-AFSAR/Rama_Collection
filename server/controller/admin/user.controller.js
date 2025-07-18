1e3b82dcf9e67561da4d2a85b07c57b8bd6026ed
Adib Afsar <c-adib-afsar@timesgroup.com>
2025-07-18 19:45:02 +0530

    Forgot pass 1.4

--- server/controller/admin/user.controller.js
+++ server/controller/admin/user.controller.js
@@ -95,7 +95,7 @@ const forgotPassword = async (req, res) => {
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
   });
 
-  const resetUrl = `${process.env.REACT_APP_API_URL}/reset-password/${token}`;
+  const resetUrl = `${process.env.DOMAIN_URL}/reset-password/${token}`;
 
   await transporter.sendMail({
     from: process.env.ADMIN_EMAIL,
