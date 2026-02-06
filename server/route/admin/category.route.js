c6c7b114422d6e3dc8d8777b90c3c46279643774
Adib Afsar <c-adib-afsar@timesgroup.com>
2026-02-06 16:51:00 +0530

    update sub cat handler 0.2

--- server/route/admin/category.route.js
+++ server/route/admin/category.route.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const { getCategories, storeCategories, updateCategories, deleteCategories, getMenuCategories } = require('../../controller/admin/category.controller')
+const { getCategories, storeCategories, updateCategories, deleteCategories, getCategoryTree } = require('../../controller/admin/category.controller')
 const upload = require('../../middleware/multer.middleware')
 const router = express.Router()
 
 
 router.get('/',getCategories)
-router.get('/tree', getMenuCategories);
+router.get('/tree', getCategoryTree);
 router.post('/store',upload.single('image'),storeCategories),
 router.post('/update/:id',upload.single('image'),updateCategories)
 router.delete('/delete/:id',deleteCategories)
