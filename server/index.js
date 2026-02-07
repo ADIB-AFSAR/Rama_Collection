71d4f235c1db3fd00afe02819e3c1d427795cb22
Adib Afsar <c-adib-afsar@timesgroup.com>
2026-02-07 15:40:59 +0530

    update SCH 0.10

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ mongoose
 
 app.get('/api/admin/product',getProducts);
 app.use('/api/wishlist', wishlistRoutes)
-app.get('/api/category', publicCategoryRoutes);
+app.use('/api/category', publicCategoryRoutes);
 app.use("/api/admin", adminRoute);
 app.use("/api", frontRoutes);
 app.get("/api/health", (req, res) => {
